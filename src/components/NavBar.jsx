42fb6626c8ffbed5b81e5a13265b43e8e6a0987b
LivEva <guests@hotmail.co.uk>
2024-10-23 12:59:04 +0100

    Adding additional styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,13 @@
-
-
 import { Link, NavLink } from "react-router-dom";
 import logo from '../assets/logo.png'
-
+import '../styles/navbar.css'
 
 
 const NavBar = () => {
 
     return (
 
-    <nav id="navbar-container">
+    <nav className="navbar-container">
 
         <NavLink to={`/articles`}> <img src={logo} id="logo"/></NavLink>
 
