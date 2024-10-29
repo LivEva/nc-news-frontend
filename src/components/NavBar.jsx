e23c7f88736446d70ba599272f3e868145683ba7
LivEva <guests@hotmail.co.uk>
2024-10-29 11:10:51 +0000

    Adding additional styling to fonts

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,51 @@
-import { Link, NavLink } from "react-router-dom";
-import logo from '../assets/logo.png'
-import '../styles/navbar.css'
-
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
+    const navigate = useNavigate();
 
     return (
+        <AppBar position="static" color="primary">
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                {/* Logo */}
+                <Box>
+                    <Button
+                    component={RouterLink}
+                    to={('/articles')}
+                    color="inherit"
+                    sx={{ mx: 1 }}
+                    id="logo"
+                    >
+                        THATS THE TEA
 
-    <nav className="navbar-container">
+                    </Button>
+                    
+                {/* Navigation Buttons */}
+                </Box>
+                <Box>
+                    <Button
+                        className="nav-button"
+                        component={RouterLink}
+                        to="/articles"
+                        color="inherit"
+                        sx={{ mx: 1 }}
+                    >
+                        Home
+                    </Button>
+                    <Button
+                        className="nav-button"
+                        component={RouterLink}
+                        to="/topics"
+                        color="inherit"
+                        sx={{ mx: 1 }}
+                    >
+                        Tea Selection
+                    </Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
-        <NavLink to={`/articles`}> <img src={logo} id="logo"/></NavLink>
-
-        <button className="nav-buttons"><Link to="/articles">Home</Link></button>
-
-        <button className="nav-buttons"><Link to="/topics">Tea selection</Link></button>
-     
-    </nav>
-
-  
-    )
-
-}
-
-export default NavBar
-
+export default NavBar;
